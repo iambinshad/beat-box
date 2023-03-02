1231b4ef4384ec184b6d13fe565a1355eb48f6f5
Mohammed Binshad <binshadp999@gmail.com>
2023-03-02 17:57:13 +0530

    Provider statemanagement applied

---
+++ lib/provider/bottom_nav_provider/bottom_nav_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomNavProv extends ChangeNotifier {
+  int currentSelectedIndex = 0;
+
+  void bottomSwitching(index) {
+    currentSelectedIndex = index;
+    notifyListeners();
+  }
+}
